c36a7c3ed6e6515cb098b320ec114abd013af530
dinesh1234r <dineshkumar.r2021ecea@sece.ac.in>
2024-10-08 19:25:31 +0530

    Change the name as medivault

--- doctors/src/Pages/Home.js
+++ doctors/src/Pages/Home.js
@@ -2,8 +2,18 @@ import React from 'react'
 import SideBar from './SideBar'
 import Patients from './Patients'
 import { HStack,Flex,Box } from '@chakra-ui/react';
+import {useEffect} from 'react'
+import {useNavigate} from 'react-router-dom'
 
 function Home() {
+  const navigate=useNavigate()
+  useEffect(()=>{
+    if(!localStorage.getItem('Jwt'))
+    {
+      navigate('/')
+    }
+  },[])
+
   return (
     <HStack w={'100%'} >
         <Box w={'20%'} justifyContent={'center'}>
