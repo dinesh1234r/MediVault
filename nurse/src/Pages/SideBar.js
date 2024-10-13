730eea5baba605bfd00c88336a438de52f804cf8
dinesh1234r <dineshkumar.r2021ecea@sece.ac.in>
2024-10-13 07:44:41 +0530

    password change in nurse

--- nurse/src/Pages/SideBar.js
+++ nurse/src/Pages/SideBar.js
@@ -31,7 +31,7 @@ function SideBar() {
   const handlechangepass=async()=>{
     try{
       const objectID=localStorage.getItem('Id');
-    const response=await axios.post('https://medivault.onrender.com/doctor/passchange',{oldpass,newpass,objectID},{
+    const response=await axios.post('https://medivault.onrender.com/nurse/passchange',{oldpass,newpass,objectID},{
       headers:{
         'Authorization':`Bearer ${localStorage.getItem('Jwt')}`,
         'Content-Type': 'application/json'
